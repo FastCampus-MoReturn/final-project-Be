66e0e3428ad822f885e373a7090a1aad2c2928ff
superb-jay <myride.jay@gmail.com>
2023-03-07 21:09:27 +0900

    git init

---
+++ src/main/java/com/fastcampus/finalprojectbe/FinalProjectBeApplication.java
@@ -0,0 +1,13 @@
+package com.fastcampus.finalprojectbe;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class FinalProjectBeApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(FinalProjectBeApplication.class, args);
+    }
+
+}
