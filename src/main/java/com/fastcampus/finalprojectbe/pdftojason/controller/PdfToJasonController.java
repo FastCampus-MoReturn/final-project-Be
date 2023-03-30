2975687846d45c36f6a6c8cb5e3083e83c4e9f0b
superb-jay <myride.jay@gmail.com>
2023-03-30 15:29:13 +0900

    test: testContorller clear

--- src/main/java/com/fastcampus/finalprojectbe/pdftojason/controller/PdfToJasonController.java
+++ src/main/java/com/fastcampus/finalprojectbe/pdftojason/controller/PdfToJasonController.java
@@ -1,57 +1,11 @@
 package com.fastcampus.finalprojectbe.pdftojason.controller;
 
 
-import org.apache.pdfbox.pdfparser.PDFParser;
-import org.apache.pdfbox.pdmodel.PDDocument;
-import org.apache.pdfbox.util.PDFTextStripper;
-import org.json.JSONArray;
-import org.json.JSONObject;
 import org.springframework.stereotype.Controller;
-import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.PostMapping;
-import org.springframework.web.bind.annotation.RequestParam;
-import org.springframework.web.multipart.MultipartFile;
-
-import javax.servlet.http.HttpServletResponse;
-import java.io.IOException;
 
 
 @Controller
 public class PdfToJasonController {
-
-
-    @GetMapping("/")
-    public String index() {
-        return "index";
-    }
-
-    @PostMapping("/upload")
-    public void jsonParsing(@RequestParam(value = "file", required = false) MultipartFile multipartFile, HttpServletResponse response) throws IOException {
-
-        PDFParser pdfParser = new PDFParser(multipartFile.getInputStream());
-        pdfParser.parse();
-        PDDocument document = pdfParser.getPDDocument();
-
-        PDFTextStripper pdfStripper = new PDFTextStripper();
-        String pdfText = pdfStripper.getText(document);
-        JSONObject json = new JSONObject();
-        JSONArray lines = new JSONArray();
-
-        String[] textLines = pdfText.split("\\r?\\n");
-
-        for (int i = 0; i < textLines.length; i++) {
-            JSONObject lineObject = new JSONObject();
-            lineObject.put(String.valueOf(i + 1), textLines[i].trim());
-            lines.put(lineObject);
-        }
-
-        json.put("lines", lines);
-        document.close();
-
-        response.setContentType("application/json;charset=UTF-8");
-        response.setStatus(HttpServletResponse.SC_UNAUTHORIZED);
-        response.getWriter().print(json);
-    }
 
 
 }
