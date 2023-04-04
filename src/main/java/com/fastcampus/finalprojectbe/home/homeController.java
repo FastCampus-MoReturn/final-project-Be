333e60fe5e23b1ae770c5dfa36e5e6bc8541fa14
superb-jay <myride.jay@gmail.com>
2023-04-04 19:16:12 +0900

    fix: redirect fixed

--- src/main/java/com/fastcampus/finalprojectbe/home/homeController.java
+++ src/main/java/com/fastcampus/finalprojectbe/home/homeController.java
@@ -2,15 +2,15 @@ package com.fastcampus.finalprojectbe.home;
 
 
 import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 import springfox.documentation.annotations.ApiIgnore;
 
 @Controller
-@RequestMapping(value = "/")
 @ApiIgnore
 public class homeController {
 
-    @RequestMapping("/")
+    @GetMapping("/")
     public String homeRedirect() {
         return "redirect:/swagger-ui/index.html";
     }
