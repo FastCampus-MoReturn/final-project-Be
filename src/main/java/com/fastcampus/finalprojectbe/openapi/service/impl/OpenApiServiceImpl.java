c2db5eac67d374153547ebf23b61d9c5a0c64909
superb-jay <myride.jay@gmail.com>
2023-04-06 04:47:50 +0900

    feat: tradingDetail function add seviceImpl

--- src/main/java/com/fastcampus/finalprojectbe/openapi/service/impl/OpenApiServiceImpl.java
+++ src/main/java/com/fastcampus/finalprojectbe/openapi/service/impl/OpenApiServiceImpl.java
@@ -1,9 +1,13 @@
-package com.fastcampus.finalprojectbe.publicapi.service.impl;
+package com.fastcampus.finalprojectbe.openapi.service.impl;
 
+import com.fastcampus.finalprojectbe.global.exception.NoSearchAdressException;
 import com.fastcampus.finalprojectbe.global.response.CommonResponse;
 import com.fastcampus.finalprojectbe.global.response.ResponseService;
-import com.fastcampus.finalprojectbe.publicapi.dto.TradingPriceIndexResDTO;
-import com.fastcampus.finalprojectbe.publicapi.service.PriceIndexApiService;
+import com.fastcampus.finalprojectbe.global.xml.XmlReader;
+import com.fastcampus.finalprojectbe.openapi.dto.tradingDetailResDTO;
+import com.fastcampus.finalprojectbe.openapi.dto.TradingPriceIndexResDTO;
+import com.fastcampus.finalprojectbe.openapi.service.AddressService;
+import com.fastcampus.finalprojectbe.openapi.service.OpenApiService;
 import com.fasterxml.jackson.databind.JsonNode;
 import lombok.RequiredArgsConstructor;
 import lombok.extern.slf4j.Slf4j;
@@ -11,7 +15,13 @@ import org.springframework.beans.factory.annotation.Value;
 import org.springframework.stereotype.Service;
 import org.springframework.web.reactive.function.client.WebClient;
 import org.springframework.web.util.UriComponentsBuilder;
+import org.w3c.dom.Document;
+import org.xml.sax.SAXException;
 
+import javax.xml.parsers.DocumentBuilder;
+import javax.xml.parsers.DocumentBuilderFactory;
+import javax.xml.parsers.ParserConfigurationException;
+import java.io.IOException;
 import java.net.URI;
 import java.time.LocalDate;
 import java.time.format.DateTimeFormatter;
@@ -24,9 +34,11 @@ import java.util.stream.StreamSupport;
 @Service
 @Slf4j
 @RequiredArgsConstructor
-public class PriceIndexApiServiceImpl implements PriceIndexApiService {
+public class OpenApiServiceImpl implements OpenApiService {
 
     private final ResponseService responseService;
+    private final AddressService addressService;
+    private final XmlReader xmlReader;
 
     @Value("${api.servicekey}")
     private String SERVICE_KEY;
@@ -36,7 +48,7 @@ public class PriceIndexApiServiceImpl implements PriceIndexApiService {
     String formattedDate = currentDate.format(formatter);
 
     @Override
-    public CommonResponse tradingPriceIndex(String regionCode, String contractType, String researchDate) {
+    public CommonResponse tradingPriceIndex(String regionCode, String contractType, int researchDate) {
 
         UriComponentsBuilder builder = UriComponentsBuilder.newInstance()
                 .scheme("https")
@@ -55,6 +67,58 @@ public class PriceIndexApiServiceImpl implements PriceIndexApiService {
         URI uri = URI.create(builder.build().toUriString() + ("&serviceKey=" + SERVICE_KEY));
 //        log.info("uri : {}", uri);
         return responseService.getListResponse(researchDateSortReturn(uri));
+    }
+
+    @Override
+    public CommonResponse tradingDetail(String address, int researchDate) throws ParserConfigurationException, IOException, SAXException {
+        String jibun = "";
+        String aptName = "";
+        try {
+            jibun = addressService.findJibun(address);
+            aptName = addressService.findAptName(address);
+            tradingDetailGetApi(address, researchDate);
+        }catch (Exception e) {
+            throw new NoSearchAdressException();
+        }
+        List<tradingDetailResDTO> aptList = xmlReader.getAptList();
+        List<tradingDetailResDTO> result = new ArrayList<>();
+        for (tradingDetailResDTO apt : aptList) {
+            if (apt.getJibun().equals(jibun) && apt.getTradeAptName().contains(aptName)) {
+                result.add(apt);
+            }
+        }
+        return responseService.getListResponse(result);
+    }
+
+    public void tradingDetailGetApi(String address, int researchDate) throws ParserConfigurationException, IOException, SAXException {
+
+        DateTimeFormatter formatter = DateTimeFormatter.ofPattern("yyyyMM");
+        String siGunGuCode = addressService.searchsiGunGuCodeCode(address);
+
+        for (int i = 0; i < researchDate; i++) {
+
+            LocalDate date = currentDate.minusMonths(i);
+            String period = date.format(formatter);
+
+            UriComponentsBuilder uribuilder = UriComponentsBuilder.newInstance()
+                    .scheme("http")
+                    .host("openapi.molit.go.kr")
+                    .path("/OpenAPI_ToolInstallPackage/service/rest/RTMSOBJSvc/getRTMSDataSvcAptTradeDev")
+                    .queryParam("serviceKey", SERVICE_KEY)
+//                .queryParam("pageNo", 1)
+                    .queryParam("numOfRows", "500")
+                    .queryParam("LAWD_CD", siGunGuCode)
+                    .queryParam("DEAL_YMD", period);
+
+            URI uri = URI.create(uribuilder.build().toUriString());
+//            log.info("uri : {}", uri);
+
+            DocumentBuilderFactory dbFactoty = DocumentBuilderFactory.newInstance();
+            DocumentBuilder dBuilder = dbFactoty.newDocumentBuilder();
+            Document doc = dBuilder.parse(uri.toString());
+            xmlReader.parser(doc);
+        }
+
     }
 
     public List<TradingPriceIndexResDTO> researchDateSortReturn(URI uri) {
@@ -87,9 +151,9 @@ public class PriceIndexApiServiceImpl implements PriceIndexApiService {
 
     }
 
-    public String createResearchDate(String researchDate) {
+    public String createResearchDate(int researchDate) {
         DateTimeFormatter formatter = DateTimeFormatter.ofPattern("yyyyMM");
-        LocalDate date = currentDate.minusMonths(Long.parseLong(researchDate));
+        LocalDate date = currentDate.minusMonths(researchDate);
         return date.format(formatter);
     }
 
