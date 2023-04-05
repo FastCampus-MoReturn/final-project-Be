21c1717dae60b73ed34d3db8c961c9e99c413f27
superb-jay <myride.jay@gmail.com>
2023-04-06 04:43:01 +0900

    feat: create Address service

---
+++ src/main/java/com/fastcampus/finalprojectbe/openapi/service/AddressService.java
@@ -0,0 +1,11 @@
+package com.fastcampus.finalprojectbe.openapi.service;
+
+
+public interface AddressService {
+
+    public String searchsiGunGuCodeCode(String address);
+    public String findJibun(String address);
+    public String findAptName(String address);
+
+
+}
