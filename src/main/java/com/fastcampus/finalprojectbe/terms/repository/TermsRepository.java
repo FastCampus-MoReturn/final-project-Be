356bc7e8b356962c6e90e67368a1630bdda71203
superbe-jay <myride.jay@gmail.com>
2023-04-07 14:42:06 +0900

    Revert "등기 용어 정리 사전 API #24"

--- src/main/java/com/fastcampus/finalprojectbe/terms/repository/TermsRepository.java
+++
@@ -1,9 +0,0 @@
-package com.fastcampus.finalprojectbe.terms.repository;
-
-import com.fastcampus.finalprojectbe.terms.entity.Terms;
-import org.springframework.data.jpa.repository.JpaRepository;
-import org.springframework.stereotype.Repository;
-
-@Repository
-public interface TermsRepository extends JpaRepository<Terms, Integer> {
-}
