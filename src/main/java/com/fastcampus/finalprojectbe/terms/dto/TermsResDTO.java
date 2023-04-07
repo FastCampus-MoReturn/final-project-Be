2c5a34d72ac8687cc0441967ba5a885387dbc87b
baker-lee <shishi1340@gmail.com>
2023-04-07 21:12:05 +0900

    fix: for Search Terms

---
+++ src/main/java/com/fastcampus/finalprojectbe/terms/dto/TermsResDTO.java
@@ -0,0 +1,14 @@
+package com.fastcampus.finalprojectbe.terms.dto;
+
+import lombok.*;
+
+@Getter
+@Setter
+@ToString
+@Builder
+@AllArgsConstructor
+@NoArgsConstructor
+public class TermsResDTO {
+    private String title;
+    private String description;
+}
